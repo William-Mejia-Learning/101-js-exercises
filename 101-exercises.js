6874e88d52f83591667baf724a324f0dae5f2111
Glitch (101-exercises) <none>
2022-03-13 05:12:26 +0000

    🌖🌦 Checkpoint ./101-exercises.js:37539998/233

--- 101-exercises.js
+++ 101-exercises.js
@@ -1366,7 +1366,7 @@ addToDone("Exercise 79 is correct.")
 function onlyNegativeOdds(arr){
   let count = [];
   arr.forEach(function(num){
-    if(num < 0 && num % 2 === 1){
+    if(num < 0 && num % 2 !== 0){
       count.push(num);
     }
   });
@@ -1384,6 +1384,13 @@ addToDone("Exercise 80 is correct.")
 // Exercise 81
 // Write a function definition named shortestString that takes in an array of strings and returns the shortest string in the array.
 
+function shortestString(arr){
+  return arr.reduce((a, b) => a.length <= b.length ? a : b);
+}
+
+
+
+
 assert(shortestString(["kiwi", "mango", "strawberry"]), "kiwi", "Exercise 81");
 assert(shortestString(["hello", "everybody"]), "hello", "Exercise 81");
 assert(shortestString(["mary", "had", "a", "little", "lamb"]), "a", "Exercise 81");
@@ -1392,6 +1399,10 @@ addToDone("Exercise 81 is correct.")
 
 // Exercise 82
 // Write a function definition named longestString that takes in sequence of strings and returns the longest string in the array.
+
+function longesttString(arr){
+  return arr.reduce((a, b) => a.length <= b.length ? a : b);
+}
 
 assert(longestString(["kiwi", "mango", "strawberry"]), "strawberry", "Exercise 82");
 assert(longestString(["hello", "everybody"]), "everybody", "Exercise 82");
